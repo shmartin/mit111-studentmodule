015a0d762de981b41ac84fe127680cef5fb692c0
shmartin <abadjuanmartin@gmail.com>
2025-04-26 11:35:50 +0800

    initialized model creation remaining apps

--- studentmodule/submission/models.py
+++ studentmodule/submission/models.py
@@ -8,3 +8,22 @@ class Document(models.Model):
 
     def __str__(self):
         return self.dtitle
+
+
+class Document_detail(models.Model):
+    did = models.ForeignKey(Document, on_delete=models.CASCADE)
+    dauthor = models.Foreignkey(User, on_delete=models.CASCADE)
+    dadviser = models.CharField(max_length=100)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.did} | {self.author} | {self.dadviser}'
+
+class Document_evaluation(models.Model):
+    did = models.ForeignKey(Document, on_delete=models.CASCADE)
+    dauthor = models.Foreignkey(User, on_delete=models.CASCADE)
+    feedback = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.did} | {self.status}'
--- studentmodule/users/models.py
+++ studentmodule/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=55)
+    lastname = models.CharField(max_length=55)
+    email = models.EmailField(max_length=55)
+    password = models.CharField(max_length=55)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+
+class Program(models.Model):
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.description
