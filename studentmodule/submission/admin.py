32431bd6230e203328b8db0fe9c250c69a81d7b9
shmartin <abadjuanmartin@gmail.com>
2025-05-01 18:52:47 +0800

    registered models

--- studentmodule/submission/admin.py
+++ studentmodule/submission/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Document
+from .models import Document, Document_detail, Document_evaluation
 
 # Register your models here.
 
 admin.site.register(Document)
+admin.site.register(Document_detail)
+admin.site.register(Document_evaluation)
