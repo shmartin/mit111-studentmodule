fc5f8ae43ca3b8d907b61d0fb1ee05037716ea3c
jcm984508 <jcm984508@slu.edu.ph>
2025-05-12 12:16:52 +0800

    Update tests.py

--- studentmodule/submission/tests.py
+++ studentmodule/submission/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HelpPageTest(TestCase):
+    def test_help_page_status_code(self):
+        response = self.client.get(reverse('help'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_help_page_template_used(self):
+        response = self.client.get(reverse('help'))
+        self.assertTemplateUsed(response, 'submission/help.html')
