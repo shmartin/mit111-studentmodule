022335bba3bae3bcc084ce2a0705a39a901dd81c
jcm984508 <jcm984508@slu.edu.ph>
2025-05-12 11:40:03 +0800

    Update urls.py

--- studentmodule/submission/urls.py
+++ studentmodule/submission/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.submission,name='submission'),
     path('guidelines/',views.guidelines,name='guidelines'),
+    path('help/', views.help_page, name='help_page'),
 ]
