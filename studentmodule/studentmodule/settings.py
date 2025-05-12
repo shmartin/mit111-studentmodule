69d40de7a23da37b908e01f1707f881b923e4cf6
jcm984508 <jcm984508@slu.edu.ph>
2025-05-12 15:57:10 +0800

    Update settings.py

--- studentmodule/studentmodule/settings.py
+++ studentmodule/studentmodule/settings.py
@@ -128,3 +128,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Use the custom User model
+AUTH_USER_MODEL = 'users.User'
