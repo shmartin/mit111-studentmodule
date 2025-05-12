d37651f4a6b837a45dd3804937796fb7b64b6424
jcm984508 <jcm984508@slu.edu.ph>
2025-05-12 17:08:16 +0800

    Update forms.py

--- studentmodule/users/forms.py
+++ studentmodule/users/forms.py
@@ -1,1 +1,21 @@
+# users/forms.py
+from django import forms
+from django.contrib.auth.models import User
+
+class UserSignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), label='Password', min_length=6)
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), label='Confirm Password', min_length=6)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError('Passwords do not match')
+        return cleaned_data
 
