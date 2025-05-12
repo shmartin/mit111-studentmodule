913c9839212cd8dded4e801bd207448a7acbb0cc
jcm984508 <jcm984508@slu.edu.ph>
2025-05-12 14:42:53 +0800

    Update urls.py

--- studentmodule/studentmodule/urls.py
+++ studentmodule/studentmodule/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('submission/', include('submission.urls')),
-    path('registration/', include('users.urls')),
+    path('users/', include('users.urls')),
 ]
