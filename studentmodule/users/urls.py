b91721d3a297b6a2a25ce82b2e5cef04714039f5
jcm984508 <jcm984508@slu.edu.ph>
2025-05-12 19:47:23 +0800

    Update urls.py

--- studentmodule/users/urls.py
+++ studentmodule/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.custom_login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('password_reset/', views.password_reset_view, name='password_reset'),
+    path('logout/', views.custom_logout_view, name='logout'),  # Custom logout
+    path('password_reset/', views.password_reset_request_view, name='password_reset'),
+    path('password_reset/done/', views.password_reset_done_view, name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.password_reset_confirm_view, name='password_reset_confirm'),
+    path('reset/done/', views.password_reset_complete_view, name='password_reset_complete'),
     path('signup/', views.signup_view, name='signup'),
 ]
