15734f611e2f046a98703147f6977fcc95783bf3
shmartin <abadjuanmartin@gmail.com>
2025-05-14 21:17:07 +0800

    removed files

--- studentmodule/submission/migrations/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Document, DocumentDetails, DocumentEvaluation, DocumentReviewers, DocumentSubmissionLog, Program
-
-admin.site.register(Document)
-admin.site.register(DocumentDetails)
-admin.site.register(DocumentEvaluation)
-admin.site.register(DocumentReviewers)
-admin.site.register(DocumentSubmissionLog)
-admin.site.register(Program)
--- studentmodule/submission/migrations/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubmissionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'submission'
